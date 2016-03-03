3fa7a1ef60e5b5208155529df5b11877c06657cc
jiangbianwanghai <webmaster@jiangbianwanghai.com>
2016-03-03 15:42:22 +0800

    Fixed a bug that can't change issue level

--- application/controllers/issue.php
+++ application/controllers/issue.php
@@ -391,6 +391,7 @@ class issue extends CI_Controller {
         $this->load->model('Model_issue', 'issue', TRUE);
         $post = array(
             'id' => $this->input->post('id'),
+            'level' => $this->input->post('level'),
             'issue_name' => $this->input->post('issue_name'),
             'url' => $this->input->post('issue_url'),
             'issue_summary' => $this->input->post('issue_summary')
