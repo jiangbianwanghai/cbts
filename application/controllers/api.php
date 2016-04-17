46e87ce9f887e613bd1e3296fc60c8fd0ae5eb96
cnxzcxy <cnxzcxy@gmail.com>
2016-04-17 11:21:35 +0800

    增加一个临时的接口输出，方便capistrano发布的时候，检查环境是否被占用，直接返回的inuse或nouse，比较ugly

---
+++ application/controllers/api.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class api extends CI_Controller {
+
+    /**
+     *  输出项目状态
+     */
+    public function check_status() {
+        $repo = $this->uri->segment(4, 0);
+        $env = (int)$this->uri->segment(6, 0);
+        $this->load->database();
+        $sql = "select ct.id from choc_test as ct join choc_repos as cr on ct.repos_id = cr.id where cr.repos_name = '" . $repo . "' and ct.rank = '" . $env . "' and ct.state =1 limit 1";
+        $query = $this->db->query($sql);
+        $rs = $query->row_array();
+        if($rs){
+            echo 'inuse';
+        }else{
+            echo 'nouse';
+        }
+    }
+
+}
