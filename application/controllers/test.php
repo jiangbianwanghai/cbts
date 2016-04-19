4416ebd9d8156979754a4178dc3e2a210cdd1baf
jiangbianwanghai <webmaster@jiangbianwanghai.com>
2016-04-19 10:31:53 +0800

    增加api

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -7,6 +7,9 @@ class test extends CI_Controller {
      */
     public function add() {
     	$data['PAGE_TITLE'] = '提交代码';
+        $aclUsers = array('71','72','69','60','73','25','30','18','19');//只允许吕云毅，彭明明，张智龙，李奎，师旭
+        if (!in_array($this->input->cookie('uids'), $aclUsers))
+            exit('只有各个绩效圈的Leader才有提交测试的权限，请联系@吕云毅，@彭明明，@张智龙，@李奎，@师旭');
         $id = $this->uri->segment(3, 0);
         $this->load->model('Model_issue', 'issue', TRUE);
         $data['row'] = $this->issue->fetchOne($id);
--- application/hooks/acl.php
+++ application/hooks/acl.php
@@ -9,7 +9,7 @@ class acl{
     
     public function index()
     {
-        if ($this->CI->uri->segment(1) != 'admin' && $this->CI->uri->segment(2) != 'signin') {
+        if ($this->CI->uri->segment(1) != 'admin' && $this->CI->uri->segment(2) != 'signin' && $this->CI->uri->segment(1) != 'api' && $this->CI->uri->segment(2) != 'check_status') {
             $this->CI->load->helper('url');
             if (!$this->CI->input->cookie('uids')) {
                 redirect('/admin/signin', 'location');
