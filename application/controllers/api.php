a52cf368bfcf405af6e9af4a088875d93ad01a31
cnxzcxy <cnxzcxy@gmail.com>
2016-05-06 11:57:49 +0800

    update api

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -9,11 +9,11 @@ class api extends CI_Controller {
         $repo = $this->uri->segment(4, 0);
         $env = (int)$this->uri->segment(6, 0);
         $this->load->database();
-        $sql = "select ct.id from choc_test as ct join choc_repos as cr on ct.repos_id = cr.id where cr.repos_name = '" . $repo . "' and ct.rank = '" . $env . "' and ct.state =1 limit 1";
+        $sql = "select ct.id, ct.issue_id from choc_test as ct join choc_repos as cr on ct.repos_id = cr.id where cr.repos_name = '" . $repo . "' and ct.rank = '" . $env . "' and ct.state =1 limit 1";
         $query = $this->db->query($sql);
         $rs = $query->row_array();
         if($rs){
-            echo 'inuse';
+            echo 'inuse' . $rs['issue_id'];
         }else{
             echo 'nouse';
         }
